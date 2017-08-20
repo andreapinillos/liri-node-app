502fd795d65b8cefe25ea90c7a3d8f5f0591f5d6
Andrea Pinillos <andreapinillos@Andreas-MBP.home>
2017-08-20 19:11:12 -0400

    everything

---
+++ keys.js
@@ -0,0 +1,9 @@
+JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '49feCXQ5NR5veQir4d5fgmUua',
+  consumer_secret: 'WPsSPwx0RtDiQidXesGWRad4t76KltHVBi6mbxMapgmv0PpDC0',
+  access_token_key: '898620905466810368-PU0pMbR535uX0fyIsyF2t4PktzGUIjv',
+  access_token_secret: 'bNNGY3CXNSmyHzdG2gTZMy2svswczfoODhUR7rk3wp5Sc',
+}
